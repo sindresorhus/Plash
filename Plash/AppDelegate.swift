d0a7a146779802e642ac9c4ef3ff8c3a53d1a1d2
Sindre Sorhus <sindresorhus@gmail.com>
2020-01-10 23:50:40 +0700

    Mention it in welcome screen

--- Plash/AppDelegate.swift
+++ Plash/AppDelegate.swift
@@ -118,7 +118,16 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 		NSApp.activate(ignoringOtherApps: true)
 		NSAlert.showModal(
 			message: "Welcome to Plash!",
-			informativeText: "Plash lives in the menu bar (droplet icon). Click it and then select “Open URL…” to get started.\n\nIf you have any feedback, bug reports, or feature requests, kindly use the “Send Feedback” button in the Plash menu. We respond to all submissions and reported issues will be dealt with swiftly. It's preferable that you report bugs this way rather than as an App Store review, since the App Store will not allow us to contact you for more information."
+			informativeText:
+				"""
+				Plash lives in the menu bar (droplet icon). Click it and then select “Open URL…” to get started.
+
+				Note: Support for multiple displays is currently limited to the ability to choose which display to show the website on. Support for setting a separate website for each display is planned.
+
+				See the project page for what else is planned: https://github.com/sindresorhus/Plash/issues
+
+				If you have any feedback, bug reports, or feature requests, kindly use the “Send Feedback” button in the Plash menu. We respond to all submissions and reported issues will be dealt with swiftly. It's preferable that you report bugs this way rather than as an App Store review, since the App Store will not allow us to contact you for more information.
+				"""
 		)
 	}
 
